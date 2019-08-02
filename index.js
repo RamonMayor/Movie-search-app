60e754bb6836bf2932ec524a572942e9642e14d6
Ramon Mayor <Ramon.mayor.uk@gmail.com>
2019-08-02 16:29:09 +0200

    movie search app

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+  res.render("search");
+});
+
+app.get("/results", (req, res)=>{
+  const query = req.query.search;
+  const url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`;
+
+  request(url,(error,response,body)=>{
+    if(!error && response.statusCode == 200){
+      const data = JSON.parse(body);
+      res.render("results",{data: data});
+    }
+  });
+});
+
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, ()=>{
+  console.log("Movie app, has started!!")
+});
